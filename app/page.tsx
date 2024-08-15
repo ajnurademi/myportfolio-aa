6e3eae154c8f27d52c33bcfd4263e49803c9dccd
ajnurxhaki <ajnur.ademi@hotmail.com>
2024-08-15 19:56:19 +0200

    Created Project for new Portfolio

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Meera_Inimai } from "next/font/google";
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <main className="relative ">
+      <div>
+        <h1>hiiiii</h1>
+      </div>
+    </main>
+  );
+}
