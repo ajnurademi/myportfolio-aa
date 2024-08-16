04990873aed2e97c93db828b855d94e477d0a8a3
ajnurxhaki <ajnur.ademi@hotmail.com>
2024-08-16 09:57:01 +0200

    added hero Section | (Spotlight, Grid-Background, toogle for Theme!)

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
-import { Meera_Inimai } from "next/font/google";
-import Image from "next/image";
+import Hero from "@/components/Hero";
 
 export default function Home() {
   return (
-    <main className="relative ">
-      <div>
+    <main className="relative bg-black-100 felx justify-center  items-center flex-col overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
         <h1>hiiiii</h1>
+        <Hero />
       </div>
     </main>
   );
