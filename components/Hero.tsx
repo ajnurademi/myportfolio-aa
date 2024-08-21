3fa20ef875677d3f3a974f6a8a5de1cee106c76b
ajnurxhaki <ajnur.ademi@hotmail.com>
2024-08-21 18:07:35 +0200

    Fixed Error | Finished About Section

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -6,14 +6,28 @@ import { FaLocationArrow } from 'react-icons/fa'
 const Hero = () => {
   return (
     <div className="pb-20 pt-36">
+      
       <div>
-        <Spotlight className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen" fill="white" />
-        <Spotlight className="top-10 left-full h-[80vh] w-[50vw]" fill="purple" />
-        <Spotlight className="top-28 left-80 h-[80vh] w-[50vw]" fill="blue" />
+        <Spotlight
+          className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
+          fill="white"
+        />
+        <Spotlight
+          className="h-[80vh] w-[50vw] top-10 left-full"
+          fill="purple"
+        />
+        <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="blue" />
       </div>
 
-      <div className="h-screen w-full dark:bg-black-100 bg-white  dark:bg-grid-white/[0.03] bg-grid-black/[0.2] flex items-center justify-center absolute top-p left-0">
-        <div className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100 bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"></div>
+      
+      <div
+        className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
+       absolute top-0 left-0 flex items-center justify-center"
+      >
+        <div
+          className="absolute pointer-events-none inset-0 flex items-center justify-center dark:bg-black-100
+         bg-white [mask-image:radial-gradient(ellipse_at_center,transparent_20%,black)]"
+        />
       </div>
 
       <div className="flex justify-center relative my-20 z-10">
@@ -22,13 +36,14 @@ const Hero = () => {
             Dynamic Web Magic with Next.js
           </p>
 
+        
           <TextGenerateEffect
             words="Transforming Concepts into Seamless User Experiences"
             className="text-center text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-center tracking-wider mb-4 text-sm md:text-lg lg:text-2xl text-white">
-            Hi I&apos; ajnur laerning coding based in switzerland 
+          <p className="text-white text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+            Hi! I&apos;m Ajnur, a little programmer based in Switzerland
           </p>
 
           <a href="#about">
@@ -38,15 +53,11 @@ const Hero = () => {
               position="right"
             />
           </a>
-
-
         </div>
       </div>
-
-
-
     </div>
-  )
-}
+  );
+};
+
 
 export default Hero
