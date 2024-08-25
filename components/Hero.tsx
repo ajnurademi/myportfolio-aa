a648a27b107cb8365dd47f2e5ff73cbaebed4623
ajnurxhaki <ajnur.ademi@hotmail.com>
2024-08-25 12:50:51 +0200

    added hero Potrait

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,12 +1,11 @@
-import { Spotlight } from './ui/Spotlight'
-import { TextGenerateEffect } from './ui/TextGenerate-Effect'
-import MagicButton from './ui/MagicButton'
-import { FaLocationArrow } from 'react-icons/fa'
+import { Spotlight } from './ui/Spotlight';
+import { TextGenerateEffect } from './ui/TextGenerate-Effect';
+import MagicButton from './ui/MagicButton';
+import { FaLocationArrow } from 'react-icons/fa';
 
 const Hero = () => {
   return (
-    <div className="pb-20 pt-36">
-      
+    <div className="pb-8 pt-36 relative">
       <div>
         <Spotlight
           className="-top-40 -left-10 md:-left-32 md:-top-20 h-screen"
@@ -19,7 +18,6 @@ const Hero = () => {
         <Spotlight className="left-80 top-28 h-[80vh] w-[50vw]" fill="purple" />
       </div>
 
-      
       <div
         className="h-screen w-full dark:bg-black-100 bg-white dark:bg-grid-white/[0.03] bg-grid-black-100/[0.2]
        absolute top-0 left-0 flex items-center justify-center"
@@ -30,19 +28,29 @@ const Hero = () => {
         />
       </div>
 
-      <div className="flex justify-center relative my-20 z-10">
-        <div className="max-w-[89vw] md:max-w-2xl lg:max-w-[60vw] flex flex-col items-center justify-center">
-          <p className="uppercase tracking-widest text-xs text-center text-blue-100 max-w-80">
+      {/* Content Section */}
+      <div className="flex flex-col md:flex-row md:flex-row-reverse justify-center items-center relative z-10 my-5 w-full max-w-screen-xl mx-auto px-4">
+        {/* Image Section */}
+        <div className="md:w-1/2 flex justify-center md:justify-end">
+          <img
+            src="/hero-portfolio.png"
+            alt="Ajnur's Portrait"
+            className="w-3/4 md:w-[500px] border-4 border-neutral-200 dark:border-slate-800 rounded-3xl"
+          />
+        </div>
+
+        {/* Left Text Section */}
+        <div className="md:w-1/2 flex flex-col items-center md:items-start mt-10 md:mt-0">
+          <p className="uppercase tracking-widest text-xs text-center md:text-left text-blue-100">
             My Portfolio Website
           </p>
 
-        
           <TextGenerateEffect
             words="Turning ideas into transformative Solutions."
-            className="text-center text-[40px] md:text-5xl lg:text-6xl"
+            className="text-center md:text-left text-[40px] md:text-5xl lg:text-6xl"
           />
 
-          <p className="text-white text-center md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
+          <p className="text-white text-center md:text-left md:tracking-wider mb-4 text-sm md:text-lg lg:text-2xl">
             Hi! I&apos;m Ajnur, a little programmer based in Switzerland
           </p>
 
@@ -59,5 +67,4 @@ const Hero = () => {
   );
 };
 
-
-export default Hero
+export default Hero;
