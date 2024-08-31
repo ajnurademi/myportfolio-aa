49cf3062a299ddea06cfc1cb765a95edfd8c12eb
ajnurxhaki <ajnur.ademi@hotmail.com>
2024-08-31 19:32:41 +0200

    fixed design Experience

--- components/Experience.tsx
+++ components/Experience.tsx
@@ -12,7 +12,7 @@ const Experience = () => {
             <div className="w-full mt-16 grid lg:grid-cols-4 grid-cols-1 gap-10">
                 {workExperience.map((card) => (
                     <Button key={card.id} duration={Math.floor(Math.random() * 10000 + 10000)} borderRadius='1.75rem' className='flex-1 text-white border-neutral-200 dark:border-slate-800'>
-                        <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2 sm:w-[34rem] w-[33rem]">
+                        <div className="flex lg:flex-row flex-col lg:items-center p-3 py-6 md:p-5 lg:p-10 gap-2 w-[33rem] lg:w-[55rem]">
                             <img src={card.thumbnail} alt={card.thumbnail} className="lg:w-32 md:w-20 w-16"/>
                             <div className="lg:ms-5">
                                 <h1 className="text-start text-xl md:text-2xl font-bold">
